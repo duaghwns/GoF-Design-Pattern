0eb02fca81da2f6f864b372dfb018f0d2cbd7274
hojoon <hj2758@gmail.com>
2021-11-20 19:57:59 +0900

    dd

--- src/PROGRAMERS/KakaoAlgorithm.java
+++ src/PROGRAMERS/KakaoAlgorithm.java
@@ -1,4 +1,48 @@
 package PROGRAMERS;
 
-public class KakaoAlgorithm {
+import java.util.*;
+
+class KakaoAlgorithm {
+    public static String[] solution(String[] record) {
+        List<String> inputStr = new ArrayList<>();
+        List<String[]> rsltStr = new ArrayList<>();
+        List<String> rsltAnswer = new ArrayList<>();
+        Map<String,String> user = new HashMap<>();
+
+        for(int i=0;i< record.length;i++) {
+            inputStr.add(record[i]);
+            rsltStr.add(inputStr.get(i).split(" "));
+        }
+
+        for (int i=0;i<inputStr.size();i++) {
+            if(rsltStr.get(i)[0].equals("Enter")||rsltStr.get(i)[0].equals("Change")){
+                user.put(rsltStr.get(i)[1],rsltStr.get(i)[2]);
+            }
+        }
+
+        for (int i=0;i<inputStr.size();i++) {
+            if(rsltStr.get(i)[0].equals("Enter")){
+                rsltAnswer.add(user.get(rsltStr.get(i)[1]) + "님이 들어왔습니다.");
+            } else if(rsltStr.get(i)[0].equals("Leave")){
+                rsltAnswer.add(user.get(rsltStr.get(i)[1]) + "님이 나갔습니다.");
+            } else {
+                continue;
+            }
+        }
+
+        String[] answer = new String[rsltAnswer.size()];
+        int size = 0;
+        for(String dd:rsltAnswer){
+            answer[size++] = dd;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] asdf = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+
+        for(String dd:solution(asdf)){
+            System.out.println(dd);
+        }
+    }
 }
