50fe7c5708ec5ee537baaad34bbcbbc49ea89343
duaghwns <hj2758@gmail.com>
2021-12-14 14:16:44 +0900

    Stack문제 v2

---
+++ src/BOJ/Silver/BOJ10828_v2.java
@@ -0,0 +1,48 @@
+package BOJ.Silver;
+
+import java.io.*;
+import java.util.Stack;
+
+public class BOJ10828_v2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
+        int num = Integer.parseInt(br.readLine());
+        Stack stack = new Stack();
+        for(int i=0;i<num;i++){
+            String[] input = br.readLine().split(" ");
+            switch (input[0]){
+                case "push":
+                    stack.push(input[1]);
+                    break;
+                case "pop":
+                    if(stack.empty()){
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(stack.pop()+"\n");
+                    }
+                    break;
+                case "size":
+                    bw.write(stack.size()+"\n");
+                    break;
+                case "empty":
+                    if(stack.empty()){
+                        bw.write("1\n");
+                    } else {
+                        bw.write("0\n");
+                    }
+                    break;
+                case "top":
+                    if(stack.empty()){
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(stack.peek()+"\n");
+                    }
+                    break;
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
