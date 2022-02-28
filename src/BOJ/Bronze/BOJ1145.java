4a694f7d36d005a021b95904f95349a599d215f6
duaghwns <hj2758@gmail.com>
2022-02-28 17:22:49 +0900

    임시저장

--- src/BOJ/Bronze/BOJ1145.java
+++ src/BOJ/Bronze/BOJ1145.java
@@ -1,9 +1,6 @@
 package BOJ.Bronze;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class BOJ1145 {
     /*
@@ -12,11 +9,11 @@ public class BOJ1145 {
      다섯 개의 자연수가 있다. 이 수의 적어도 대부분의 배수는 위의 수 중 적어도 세 개로 나누어 지는 가장 작은 자연수이다.
       서로 다른 다섯 개의 자연수가 주어질 때, 적어도 대부분의 배수를 출력하는 프로그램을 작성하시오.
     */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] arr = Integer.parseInt(br.readLine().split(" "));
+        String[] arr = br.readLine().split(" ");
 
 
 
