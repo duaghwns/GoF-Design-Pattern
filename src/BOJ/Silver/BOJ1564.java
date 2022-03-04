3777d1e1f43a56efa881e08aa96dc3983da5e758
duaghwns <hj2758@gmail.com>
2022-03-04 17:22:17 +0900

    메모리 초과.. 이유가 뭔가요?

--- src/BOJ/Silver/BOJ1564.java
+++ src/BOJ/Silver/BOJ1564.java
@@ -3,37 +3,39 @@ package BOJ.Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.Stack;
 
 public class BOJ1564 {
-    private static Long factorial(int num){
-        Long rst = 1L;
-        for(Long i=1L;i<=num;i++){
-            rst *= i;
-        }
-        return rst;
-    }
-
-    private static int findByIndex(String str){
-        int inx = 0;
-        for(int i=str.length()-1;i>=0;i--) {
-            if(str.charAt(i) != '0'){
-                return i;
-            }
-        }
-        return inx;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputVal = br.readLine();
+        br.close();
+
+        // Factorial
+        BigInteger big = BigInteger.valueOf(1);
+
+        for(int i=1;i<=Integer.parseInt(inputVal);i++){
+            big = big.multiply(BigInteger.valueOf(i));
+        }
+
+        // findByIndex
+        int index = 0;
+
+        for(int i=String.valueOf(big).length()-1;i>=0;i--) {
+            if(String.valueOf(big).charAt(i) != '0') {
+                index = i;
+                break;
+            }
+        }
+
         Stack stack = new Stack();
         StringBuffer rst = new StringBuffer();
-        String input = String.valueOf(factorial(Integer.parseInt(br.readLine())));
-        int index = findByIndex(input);
 
         for(int i=0;i<=index;i++){
-           stack.push(input.charAt(i));
+            stack.push(String.valueOf(big).charAt(i));
         }
+
         for(int i=0;i<5;i++){
             if(!stack.empty()) rst.append(stack.pop());
         }
