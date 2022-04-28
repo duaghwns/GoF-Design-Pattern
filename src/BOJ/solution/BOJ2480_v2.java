fb557c05b7b7e12cb572f5bc4553e3749ee24b75
duaghwns <hj2758@gmail.com>
2022-04-29 00:55:01 +0900

    주사위 세개

--- src/BOJ/solution/BOJ2480_v2.java
+++ src/BOJ/solution/BOJ2480_v2.java
@@ -3,32 +3,32 @@ package BOJ.solution;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class BOJ2480_v2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cnt = 1;
-        int bigger = 0, sameNum = 0, money = 0;
         String[] input = br.readLine().split(" ");
+        int[] numbers = Arrays.stream(input).mapToInt(Integer::parseInt).toArray();
+        int bigger = 0, sameNum = 0, money = 0, cnt = 1, i = 0;
 
-        int i=0;
-        while(i<input.length){
+        while(i<3) {
             if(i<2){
-                if(input[i]==input[i+1]){
-                    sameNum = Integer.parseInt(input[i]);
+                if(numbers[i]==numbers[i+1]){
+                    sameNum = numbers[i];
                     cnt++;
                 }
-                bigger = i>bigger?Integer.parseInt(input[i]):bigger;
             }
+            bigger = numbers[i] > bigger ? numbers[i] : bigger;
             i++;
         }
 
         switch(cnt){
-            case 3: money += sameNum * 1000 + 10000; break;
-            case 2: money += sameNum * 100 + 1000; break;
+            case 3: money += (sameNum * 1000) + 10000; break;
+            case 2: money += (sameNum * 100) + 1000; break;
             case 1: money += bigger * 100; break;
         }
-
-        System.out.println(money);
+        System.out.print(money);
     }
 }
