cc7b058eb738667e1ff6936dfbc6cc406f9a17c7
duaghwns <hj2758@gmail.com>
2022-04-28 23:12:03 +0900

    d

---
+++ src/BOJ/Silver/BOJ1181.java
@@ -0,0 +1,13 @@
+package BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ1181 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+
+}
