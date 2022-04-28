9eb50b9c4f1621a4aaa1b7d17eea9e336e92eaf4
duaghwns <hj2758@gmail.com>
2022-04-28 17:34:08 +0900

    d

---
+++ src/BOJ/solution/BOJ2480_v2.java
@@ -0,0 +1,14 @@
+package BOJ.solution;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ2480_v2 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 1;
+        int bigger, sameNum, money = 0;
+
+
+    }
+}
