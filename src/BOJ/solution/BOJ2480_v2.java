d2e72d6b4369d1be484f8718581d0032003b082a
duaghwns <hj2758@gmail.com>
2022-04-29 16:56:34 +0900

    주사위 세개

--- src/BOJ/solution/BOJ2480_v2.java
+++ src/BOJ/solution/BOJ2480_v2.java
@@ -1,22 +1,19 @@
 package BOJ.solution;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 
 public class BOJ2480_v2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int bigger = 0, sameNum = 0, money = 0, cnt = 1, i = 0;
 
         while(i<3) {
-            if(i<2){
-                if(numbers[i]==numbers[i+1]){
-                    sameNum = numbers[i];
-                    cnt++;
-                }
+            if(i<2 && numbers[i]==numbers[i+1]){
+                sameNum = numbers[i];
+                cnt++;
             }
             bigger = numbers[i] > bigger ? numbers[i] : bigger;
             i++;
@@ -27,6 +24,8 @@ public class BOJ2480_v2 {
             case 2: money += sameNum * 100 + 1000; break;
             case 1: money += bigger * 100; break;
         }
-        System.out.print(money);
+        bw.write(money+"");
+        bw.flush();
+        bw.close();
     }
 }
