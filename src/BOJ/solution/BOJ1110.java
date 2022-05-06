34c53ce8961a296187c7b9f4fcef5fb299b85986
duaghwns <hj2758@gmail.com>
2022-05-06 22:14:42 +0900

    더하기 사이클

---
+++ src/BOJ/solution/BOJ1110.java
@@ -0,0 +1,15 @@
+package BOJ.solution;
+
+import java.io.*;
+
+public class BOJ1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int input = Integer.parseInt(br.readLine());
+
+
+    }
+}
