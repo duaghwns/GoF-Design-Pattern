7c3748f3acba5920e924455528a6ed106917825b
duaghwns <hj2758@gmail.com>
2022-05-08 12:29:13 +0900

    더하기 사이클

--- src/BOJ/solution/BOJ1110.java
+++ src/BOJ/solution/BOJ1110.java
@@ -6,10 +6,14 @@ public class BOJ1110 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        String[] arr = new String[2];
 
 
-        int input = Integer.parseInt(br.readLine());
-
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.charAt(i)+"";
+            System.out.println(arr[i]);
+        }
 
     }
 }
