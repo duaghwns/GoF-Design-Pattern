ff259fb12e1727b2829f7046cf5ef689725bdf84
duaghwns <hj2758@gmail.com>
2022-05-23 23:31:51 +0900

    신고결과받기

--- src/PROGRAMERS/solution/신고결과받기.java
+++ src/PROGRAMERS/solution/신고결과받기.java
@@ -21,19 +21,13 @@ public class 신고결과받기 {
                     map.put(user, set);
                 }
                 // 신고 당한 횟수
-//                if (user.equals(repo[1])) {
-//                    cnt.put(user, cnt.get(user)+1);
-//                }
+                if (user.equals(repo[1])) {
+                    cnt.put(user, cnt.get(user)+1);
+                }
             }
         }
 
         for (String user : id_list) {
-            for (String r : report) {
-                String[] repo = r.split(" ");
-                if (user.equals(repo[1])) {
-                    cnt.put(user, cnt.get(user) + 1);
-                }
-            }
             int c = 0;
             if (map.get(user) != null) {
                 for(String name : map.get(user)){
@@ -42,24 +36,28 @@ public class 신고결과받기 {
                     }
                 }
             }
-            answer[i] = c;
+            if (answer.length < k) {
+                answer[i] = 0;
+            } else {
+                answer[i] = c;
+            }
             i++;
         }
 
-        System.out.println(cnt);
         System.out.println(map);
+        System.out.println(cnt);
 
         return answer;
     }
 
 
     public static void main(String[] args) {
-        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
-        int k = 2;
-//        String[] id_list = {"con", "ryan"};
-//        String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
-//        int k = 3;
+//        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+//        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+//        int k = 2;
+        String[] id_list = {"con", "ryan"};
+        String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
+        int k = 3;
 
         for (int i : solution(id_list, report, k)) {
             System.out.print(i +" ");
