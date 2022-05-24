e55cf5a23a1d81d990772c8eb9ea1ad681d291aa
duaghwns <hj2758@gmail.com>
2022-05-24 17:08:07 +0900

    신고결과받기

--- src/PROGRAMERS/solution/신고결과받기.java
+++ src/PROGRAMERS/solution/신고결과받기.java
@@ -6,54 +6,6 @@ public class 신고결과받기 {
 
     public static int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
-        Map<String, Set<String>> map = new HashMap<>();
-        Map<String, Integer> cnt = new HashMap<>();
-
-        int i = 0;
-        for(String user : id_list){
-            Set<String> list = new HashSet<>();
-            cnt.put(user, 0);
-
-            for (String r : report) {
-                String[] repo = r.split(" ");
-                // 유저가 신고한 사람들
-                if(user.equals(repo[0])){
-                    list.add(repo[1]);
-                    map.put(user, list);
-                }
-                // 신고 당한 횟수
-                if (user.equals(repo[1])) {
-                    cnt.put(user, cnt.get(user)+1);
-                }
-            }
-        }
-
-        for (String user : id_list) {
-            int c = 0;
-            if (map.get(user) != null) {
-                for(String name : map.get(user)){
-                    if (cnt.get(name) >= k) {
-                        c++;
-                    }
-                }
-            }
-            if (answer.length < k) {
-                answer[i] = 0;
-            } else {
-                answer[i] = c;
-            }
-            i++;
-        }
-
-        System.out.println(map);
-        System.out.println(cnt);
-
-        return answer;
-    }
-
-
-    public static int[] solution2(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
         Map<String, List<String>> map = new HashMap<>();
         Map<String, Integer> cnt = new HashMap<>();
         Set<String> set = new HashSet<>();
@@ -106,7 +58,7 @@ public class 신고결과받기 {
 //        String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
 //        int k = 3;
 
-        for (int i : solution2(id_list, report, k)) {
+        for (int i : solution(id_list, report, k)) {
             System.out.print(i +" ");
         }
     }
