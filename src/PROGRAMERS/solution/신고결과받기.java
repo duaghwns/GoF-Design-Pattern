0dccab9dc5246cc6ebbcba68b0381db4f415bcd1
duaghwns <hj2758@gmail.com>
2022-05-24 17:46:42 +0900

    신고결과받기

--- src/PROGRAMERS/solution/신고결과받기.java
+++ src/PROGRAMERS/solution/신고결과받기.java
@@ -1,6 +1,8 @@
 package PROGRAMERS.solution;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class 신고결과받기 {
 
@@ -8,29 +10,24 @@ public class 신고결과받기 {
         int[] answer = new int[id_list.length];
         Map<String, List<String>> map = new HashMap<>();
         Map<String, Integer> cnt = new HashMap<>();
-        Set<String> set = new HashSet<>();
+        List<Object> list = Stream.of(Arrays.stream(report).distinct().toArray()).collect(Collectors.toList());
 
-        for(String repo : report){
-            set.add(repo);
-        }
-
-        for(String user : id_list){
-            cnt.put(user,0);
-            List<String> list = new ArrayList<>();
-            Iterator iter = set.iterator();
-
-            while(iter.hasNext()){
-                String[] a = iter.next().toString().split(" ");
-                if(user.equals(a[0])){
-                    list.add(a[1]);
-                    map.put(user,list);
+        for (String user : id_list) {
+            cnt.put(user, 0);
+            ArrayList li = new ArrayList();
+            for (Object item : list) {
+                String[] obj = item.toString().split(" ");
+                if(user.equals(obj[0])){
+                    li.add((obj[1]));
+                    map.put(user, li);
                 }
 
-                if (user.equals(a[1])) {
-                    cnt.put(user, cnt.get(user)+1);
+                if (user.equals(obj[1])) {
+                    cnt.put(user, cnt.get(user) + 1);
                 }
             }
         }
+
         int i=0;
         for(String user : id_list){
             int c = 0;
@@ -44,6 +41,7 @@ public class 신고결과받기 {
             answer[i] = c;
             i++;
         }
+
         System.out.println(map);
         System.out.println(cnt);
         return answer;
@@ -51,12 +49,12 @@ public class 신고결과받기 {
 
 
     public static void main(String[] args) {
-        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
-        int k = 2;
-//        String[] id_list = {"con", "ryan"};
-//        String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
-//        int k = 3;
+//        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+//        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+//        int k = 2;
+        String[] id_list = {"con", "ryan"};
+        String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
+        int k = 3;
 
         for (int i : solution(id_list, report, k)) {
             System.out.print(i +" ");
