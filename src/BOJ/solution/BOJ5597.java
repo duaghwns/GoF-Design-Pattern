06518d22d99b7e7f77dd5c627e6637184ae6e4d0
duaghwns <hj2758@gmail.com>
2022-08-06 11:04:01 +0900

    ㅏㅏ

--- src/BOJ/solution/BOJ5597.java
+++ src/BOJ/solution/BOJ5597.java
@@ -1,21 +1,25 @@
 package BOJ.solution;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class BOJ5597 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Integer> list = new ArrayList();
 
-        for (int i = 0; i < 30; i++) {
-            if(i>=28){
-                list.add(null);
-            }else {
-                list.add(sc.nextInt());
-            }
+        for (int i = 0; i < 28; i++) {
+            list.add(sc.nextInt());
         }
+
+        // 정렬
+        list.sort(Comparator.naturalOrder());
+
+        list.add(0);
+        list.add(0);
 
         int[] a = new int[30];
         for (int i = 1; i <= 30; i++) {
@@ -24,12 +28,13 @@ public class BOJ5597 {
 
 
         for (int i = 1; i <= 30; i++) {
-            if(list.get(i) == a[i]){
+            if(list.get(i) != null && list.get(i) == a[i]){
                 list.remove(list.get(i));
-            } else if(list.get(i)==null){
+            } else if(list.get(i)==0){
                 list.remove(list.get(i));
             }
         }
+
 
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
