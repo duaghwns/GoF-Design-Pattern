b7483e45e675493a0bb98984b33fffa7f6f32eb0
duaghwns <hj2758@gmail.com>
2022-08-09 16:14:53 +0900

    신고결과받기

---
+++ src/PROGRAMERS/solution/신고결과받기2.java
@@ -0,0 +1,12 @@
+package PROGRAMERS.solution;
+
+public class 신고결과받기2 {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
