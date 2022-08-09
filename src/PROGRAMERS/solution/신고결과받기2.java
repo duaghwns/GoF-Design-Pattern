4502c8f26b2f927bc2047418ef2cf382e57b02c8
duaghwns <hj2758@gmail.com>
2022-08-10 00:49:22 +0900

    신고결과받기

--- src/PROGRAMERS/solution/신고결과받기2.java
+++ src/PROGRAMERS/solution/신고결과받기2.java
@@ -1,12 +1,31 @@
 package PROGRAMERS.solution;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
 public class 신고결과받기2 {
-    public int[] solution(String[] id_list, String[] report, int k) {
+    static int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = {};
+        HashSet<String> set = new HashSet();
+        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Set> singo = new HashMap<>();
+        StringTokenizer st;
+        for (String s : report) {
+
+        }
+
+
+
         return answer;
     }
 
     public static void main(String[] args) {
+        String[] idList = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k = 2;
 
+        System.out.println(solution(idList,report,k));
     }
 }
