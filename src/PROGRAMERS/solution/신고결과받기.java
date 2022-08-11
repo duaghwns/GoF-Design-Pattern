2a01d975f39263fcb3a35b111abab6705b472eda
duaghwns <hj2758@gmail.com>
2022-08-11 15:54:14 +0900

    신고결과받기

--- src/PROGRAMERS/solution/신고결과받기.java
+++ src/PROGRAMERS/solution/신고결과받기.java
@@ -1,8 +1,6 @@
 package PROGRAMERS.solution;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class 신고결과받기 {
 
@@ -10,19 +8,19 @@ public class 신고결과받기 {
         int[] answer = new int[id_list.length];
         Map<String, List<String>> map = new HashMap<>();
         Map<String, Integer> cnt = new HashMap<>();
-
-        // 중복제거
-        List<Object> list = Stream.of(Arrays.stream(report).distinct().toArray()).collect(Collectors.toList());
-
+        Set<Object> set = new HashSet<>(List.of(report));
+        StringTokenizer st;
         for (String user : id_list) {
             // 신고받은 유저 0으로 초기화
             cnt.put(user, 0);
 
             ArrayList li = new ArrayList();
 
-            for (Object obj : list) {
+            for (Object obj : set) {
+                st = new StringTokenizer((String)obj);
+                String key = st.nextToken();
+                String singo = st.nextToken();
                 String[] item = obj.toString().split(" ");
-
                 // 사용자 신고
                 if(user.equals(item[0])){
                     li.add((item[1]));
@@ -48,7 +46,6 @@ public class 신고결과받기 {
             answer[i] = c;
             i++;
         }
-
         System.out.println(map);
         System.out.println(cnt);
         return answer;
@@ -56,12 +53,12 @@ public class 신고결과받기 {
 
 
     public static void main(String[] args) {
-        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
-        int k = 2;
-//        String[] id_list = {"con", "ryan"};
-//        String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
-//        int k = 3;
+//        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+//        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+//        int k = 2;
+        String[] id_list = {"con", "ryan"};
+        String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
+        int k = 3;
 
         for (int i : solution(id_list, report, k)) {
             System.out.print(i +" ");
--- src/PROGRAMERS/solution/신고결과받기2.java
+++ src/PROGRAMERS/solution/신고결과받기2.java
@@ -1,40 +1,37 @@
 package PROGRAMERS.solution;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class 신고결과받기2 {
-    static int[] solution(String[] id_list, String[] report, int k) {
+
+    public static int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         Map<String, List<String>> map = new HashMap<>();
         Map<String, Integer> cnt = new HashMap<>();
-
-        // 중복제거
-        List<Object> list = Stream.of(Arrays.stream(report).distinct().toArray()).collect(Collectors.toList());
+        Set<Object> set = new HashSet<>(List.of(report));
 
         for (String user : id_list) {
             // 신고받은 유저 0으로 초기화
             cnt.put(user, 0);
-
             ArrayList li = new ArrayList();
 
-            for (Object obj : list) {
-                String[] item = obj.toString().split(" ");
-
+            for (Object obj : set) {
+                StringTokenizer st = new StringTokenizer((String)obj);
+                String key = st.nextToken();
+                String singo = st.nextToken();
                 // 사용자 신고
-                if(user.equals(item[0])){
-                    li.add((item[1]));
+                if(user.equals(key)){
+                    li.add((singo));
                     map.put(user, li);
                 }
                 // 신고받은 유저 카운트
-                if (user.equals(item[1])) {
+                if (user.equals(singo)) {
                     cnt.put(user, cnt.get(user) + 1);
                 }
             }
         }
 
-        int i=0;
+        int i = 0;
         for(String user : id_list){
             int c = 0;
             if (map.get(user) != null) {
@@ -44,19 +41,24 @@ public class 신고결과받기2 {
                     }
                 }
             }
-            answer[i] = c;
-            i++;
+            answer[i++] = c;
         }
-
         System.out.println(map);
         System.out.println(cnt);
         return answer;
     }
 
+
     public static void main(String[] args) {
-        String[] idList = {"muzi", "frodo", "apeach", "neo"};
-        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
-        int k = 2;
-        System.out.println("main : " + solution(idList,report,k));
+//        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+//        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+//        int k = 2;
+        String[] id_list = {"con", "ryan"};
+        String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
+        int k = 3;
+
+        for (int i : solution(id_list, report, k)) {
+            System.out.print(i +" ");
+        }
     }
 }
