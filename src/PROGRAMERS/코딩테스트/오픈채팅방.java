7f4b6ab265790eb9bd972f964aaf79bddde5411f
duaghwns <hj2758@gmail.com>
2022-08-17 17:49:52 +0900

    오픈채팅방

--- src/PROGRAMERS/코딩테스트/오픈채팅방.java
+++ src/PROGRAMERS/코딩테스트/오픈채팅방.java
@@ -7,12 +7,11 @@ import java.util.Map;
 
 public class 오픈채팅방 {
     static String[] solution(String[] record) {
-        int len = record.length;
-        String[][] arr = new String[len][3];
+        String[][] arr = new String[record.length][3];
         List<String> rst = new ArrayList<>();
         Map<String, String> user = new HashMap<>();
 
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < record.length; i++){
             arr[i] = record[i].split(" ");
 
             if( "Enter".equals(arr[i][0]) || "Change".equals(arr[i][0]) ) {
@@ -20,7 +19,7 @@ public class 오픈채팅방 {
             }
         }
 
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < record.length; i++){
                  if("Enter".equals(arr[i][0])) rst.add(user.get(arr[i][1]) + "님이 들어왔습니다.");
             else if("Leave".equals(arr[i][0])) rst.add(user.get(arr[i][1]) + "님이 나갔습니다.");
         }
