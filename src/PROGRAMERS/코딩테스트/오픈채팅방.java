752ed51c2c4014c8d5f1d14bfb660fa036a38fba
duaghwns <hj2758@gmail.com>
2022-08-17 17:48:59 +0900

    오픈채팅방

--- src/PROGRAMERS/코딩테스트/오픈채팅방.java
+++ src/PROGRAMERS/코딩테스트/오픈채팅방.java
@@ -6,8 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class 오픈채팅방 {
-    public static String[] solution(String[] record) {
-
+    static String[] solution(String[] record) {
         int len = record.length;
         String[][] arr = new String[len][3];
         List<String> rst = new ArrayList<>();
@@ -15,24 +14,17 @@ public class 오픈채팅방 {
 
         for(int i = 0; i < len; i++){
             arr[i] = record[i].split(" ");
-            if(arr[i][0].equals("Enter")||arr[i][0].equals("Change")){
+
+            if( "Enter".equals(arr[i][0]) || "Change".equals(arr[i][0]) ) {
                 user.put(arr[i][1],arr[i][2]);
             }
         }
 
         for(int i = 0; i < len; i++){
-            if(arr[i][0].equals("Enter")){ rst.add(user.get(arr[i][1]) + "님이 들어왔습니다."); }
-            else if(arr[i][0].equals("Leave")) { rst.add(user.get(arr[i][1]) + "님이 나갔습니다."); }
+                 if("Enter".equals(arr[i][0])) rst.add(user.get(arr[i][1]) + "님이 들어왔습니다.");
+            else if("Leave".equals(arr[i][0])) rst.add(user.get(arr[i][1]) + "님이 나갔습니다.");
         }
 
-        String[] answer = new String[rst.size()];
-
-        int i=0;
-
-        for(String str : rst){
-            answer[i++] = str;
-        }
-
-        return answer;
+        return rst.toArray(new String[rst.size()]);
     }
 }
