eef8bdb7d164ebc1438dab6db6b37ccf3d5bb941
duaghwns <hj2758@gmail.com>
2022-08-19 00:23:21 +0900

    오픈시계

--- src/BOJ/solution/BOJ2525.java
+++ src/BOJ/solution/BOJ2525.java
@@ -1,7 +1,26 @@
 package BOJ.solution;
 
-public class BOJ2525 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class BOJ2525 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if (m > 45) {
+            h++;
+            m = 60 - (45 - m);
+            if (h < 0) {
+                h = 23;
+            }
+            System.out.printf("%d %d",h,m);
+        } else {
+            System.out.printf("%d %d",h,(m - 45));
+        }
     }
 }
