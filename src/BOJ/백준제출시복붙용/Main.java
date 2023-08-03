71804377fdb7d9a14dd008141e7af56266d4cc92
duaghwns <hj2758@gmail.com>
2023-08-03 17:20:45 +0900

    rudgk

---
+++ src/BOJ/백준제출시복붙용/Main.java
@@ -0,0 +1,60 @@
+package BOJ.백준제출시복붙용;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int len = str.length();
+        String rtn = "";
+
+        for (int i = 0; i < str.length() / 3; i++) {
+            rtn = convertBinaryToDecimal(str.substring(len-3 , len)) + rtn;
+            len = len -3;
+        }
+
+        if(len % 3 > 0) {
+            rtn = convertBinaryToDecimal(str.substring(0, len % 3)) + rtn;
+        }
+
+
+        System.out.println(rtn);
+    }
+
+
+    private static String convertBinaryToDecimal(String str) {
+        int a = 0;
+
+        switch (str.length()){
+            case 3:
+                if(str.charAt(0) == '1') {
+                    a += 4;
+                }
+                if(str.charAt(1)== '1'){
+                    a += 2;
+                }
+                if(str.charAt(2) == '1') {
+                    a += 1;
+                }
+                break;
+            case 2:
+                if(str.charAt(0)== '1'){
+                    a += 2;
+                }
+                if(str.charAt(1) == '1') {
+                    a += 1;
+                }
+                break;
+            case 1:
+                if(str.charAt(0) == '1') {
+                    a += 1;
+                }
+                break;
+        }
+
+        return a + "";
+    }
+}
