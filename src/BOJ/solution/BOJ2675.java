9b935384e5f56656b0f1ce72abdf56e4497ea259
duaghwns <hj2758@gmail.com>
2023-09-22 16:49:54 +0900

    BOJ2675 문자열 반복

---
+++ src/BOJ/solution/BOJ2675.java
@@ -0,0 +1,24 @@
+package BOJ.solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String str = br.readLine();
+        char[] arr = new char[str.length()];
+
+        str.getChars(0,str.length(),arr,0);
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i]);
+            }
+        }
+    }
+}
