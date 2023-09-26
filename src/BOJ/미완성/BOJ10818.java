be1c2acfda6eb31835a576f186210ceff66d6cb6
duaghwns <hj2758@gmail.com>
2023-09-26 13:19:06 +0900

    백준 10818번 하는 중

---
+++ src/BOJ/미완성/BOJ10818.java
@@ -0,0 +1,18 @@
+package BOJ.미완성;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10818 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+}
