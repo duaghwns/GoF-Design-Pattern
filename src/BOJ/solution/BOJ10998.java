c6f4ac3da61a5a27b08f27972069ef1edcf4b23b
duaghwns <hj2758@gmail.com>
2023-10-05 08:16:37 +0900

    백준 10818번 최소, 최대

--- src/BOJ/solution/BOJ10998.java
+++ src/BOJ/solution/BOJ10998.java
@@ -1,5 +1,8 @@
 package BOJ.solution;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BOJ10998 {
@@ -10,5 +13,19 @@ public class BOJ10998 {
         int b = sc.nextInt();
 
         System.out.println(a * b);
+    }
+
+    public static class BOJ10818 {
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+             int n = Integer.parseInt(br.readLine());
+
+            String[] arr = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt).sorted()
+                    .mapToObj(String::valueOf).toArray(String[]::new);
+
+            System.out.print(arr[0]+" "+arr[n-1]);
+        }
     }
 }
