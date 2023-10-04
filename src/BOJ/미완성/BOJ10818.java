d0c8c1554144da8b4e23bc55468c0f6d608a5440
duaghwns <hj2758@gmail.com>
2023-10-05 08:15:35 +0900

    백준 10818번 최소, 최대

--- src/BOJ/미완성/BOJ10818.java
+++ src/BOJ/미완성/BOJ10818.java
@@ -7,19 +7,13 @@ import java.util.Arrays;
 public class BOJ10818 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
 
-        int n = Integer.parseInt(br.readLine());
+         int n = Integer.parseInt(br.readLine());
 
         String[] arr = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt).sorted()
                 .mapToObj(String::valueOf).toArray(String[]::new);
 
-        for(int i = 0; i < n; i++) {
-            if(i==0 || i==(n-1)) {
-                sb.append(arr[i]).append(" ");
-            }
-        }
-        System.out.println(sb);
+        System.out.print(arr[0]+" "+arr[n-1]);
     }
 }
