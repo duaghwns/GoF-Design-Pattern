8785d38eb19a43804a0cc3d23891b7c5a88577d7
duaghwns <hj2758@gmail.com>
2023-11-10 11:14:06 +0900

    [백준 1157번] 단어공부

---
+++ src/BOJ/solution/BOJ1157.java
@@ -0,0 +1,39 @@
+package BOJ.solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+// https://www.acmicpc.net/problem/1157
+public class BOJ1157 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine().toUpperCase();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            if(map.get(str.charAt(i)) == null){
+                map.put(str.charAt(i), 1);
+            } else {
+                map.put(str.charAt(i), map.get(str.charAt(i))+1);
+            }
+        }
+
+        String maxKey = null;
+        int maxVal = Integer.MIN_VALUE;
+
+        for (Map.Entry<Character, Integer> ety : map.entrySet()) {
+            if(ety.getValue() > maxVal) {
+                maxKey = ety.getKey().toString();
+                maxVal = ety.getValue();
+            } else if (ety.getValue() == maxVal) {
+                maxKey = "?";
+                maxVal = ety.getValue();
+            }
+        }
+
+        System.out.print(maxKey);
+    }
+}
