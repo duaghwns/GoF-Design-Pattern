e26548f8ceba6404ad31da2923df59206542dd8e
duaghwns <hj2758@gmail.com>
2024-09-09 15:22:04 +0900

    [PCCP 기출문제] 1번/동영상 재생기

---
+++ src/PROGRAMERS/코딩테스트/PCCP_기출문제_1번_동영상_재생기.java
@@ -0,0 +1,19 @@
+package PROGRAMERS.코딩테스트;
+
+public class PCCP_기출문제_1번_동영상_재생기 {
+    public static String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String video_len = "34:33";
+        String pos = "13:00";
+        String op_start = "00:55";
+        String op_end = "02:55";
+        String[] commands = {"next", "prev"};
+
+        solution(video_len, pos, op_start, op_end, commands);
+    }
+}
